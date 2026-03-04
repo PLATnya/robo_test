240e7ba181a3f64ffa2314d0957c4d1d7b9b56c2
PLATnya <vadim.plataniy@gmail.com>
2026-03-04 11:42:03 +0000

    Add tool scripts for Docker container

---
+++ container_shell.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker exec -it ros_gz_gui bash
---
+++ docker_run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME="ros_gz_gui"
+IMAGE_NAME="exentr0/ros2-test:jazzy"
+
+if docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+    docker start ${CONTAINER_NAME}
+else
+    docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --gpus all --name ${CONTAINER_NAME} ${IMAGE_NAME}
+fi
